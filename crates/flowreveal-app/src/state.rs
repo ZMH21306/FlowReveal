bf4c341cc93a25e2ca5e13f5ee4783e24bdf6fcd
MingHao Zhou <2130606191@qq.com>
2026-05-01 19:53:41 +0800

    Update state.rs

--- crates/flowreveal-app/src/state.rs
+++ crates/flowreveal-app/src/state.rs
@@ -3,7 +3,8 @@ use tokio::sync::{mpsc, Mutex, RwLock};
 use engine_core::http_message::{HttpMessage, HttpSession};
 use engine_core::capture_config::CaptureConfig;
 use engine_core::engine_stats::{CaptureStatus, EngineStats};
-use engine_core::proxy::ProxyShutdownHandle;
+use engine_core::mitm::CaManager;
+use engine_core::platform_integration::windows::ProxySettings;
 
 pub struct AppState {
     pub sessions: Arc<RwLock<Vec<HttpSession>>>,
@@ -11,7 +12,11 @@ pub struct AppState {
     pub stats: Arc<RwLock<EngineStats>>,
     pub event_tx: Arc<Mutex<Option<mpsc::Sender<HttpMessage>>>>,
     pub config: Arc<RwLock<Option<CaptureConfig>>>,
-    pub shutdown_handle: Arc<Mutex<Option<ProxyShutdownHandle>>>,
+    pub shutdown_handle: Arc<Mutex<Option<tokio::sync::oneshot::Sender<()>>>>,
+    pub ca_manager: Arc<RwLock<Option<CaManager>>>,
+    pub original_proxy_settings: Arc<Mutex<Option<ProxySettings>>>,
+    pub proxy_was_set: Arc<RwLock<bool>>,
+    pub cert_was_installed: Arc<RwLock<bool>>,
 }
 
 impl AppState {
@@ -23,6 +28,10 @@ impl AppState {
             event_tx: Arc::new(Mutex::new(None)),
             config: Arc::new(RwLock::new(None)),
             shutdown_handle: Arc::new(Mutex::new(None)),
+            ca_manager: Arc::new(RwLock::new(None)),
+            original_proxy_settings: Arc::new(Mutex::new(None)),
+            proxy_was_set: Arc::new(RwLock::new(false)),
+            cert_was_installed: Arc::new(RwLock::new(false)),
         }
     }
 }
