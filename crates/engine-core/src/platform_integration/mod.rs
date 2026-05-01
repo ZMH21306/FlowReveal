09e18856de07043f6f394a12674fd0bc1dd331c6
MingHao Zhou <2130606191@qq.com>
2026-05-01 21:55:54 +0800

    Update mod.rs

--- crates/engine-core/src/platform_integration/mod.rs
+++ crates/engine-core/src/platform_integration/mod.rs
@@ -1,5 +1,11 @@
 #[cfg(target_os = "windows")]
 pub mod windows;
 
+#[cfg(target_os = "windows")]
+pub mod api_hook;
+
 #[cfg(not(target_os = "windows"))]
 pub mod stub;
+
+#[cfg(not(target_os = "windows"))]
+pub mod api_hook;
