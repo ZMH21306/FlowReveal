8351b3597da2e9bce1df66077b435b214b6fca68
MingHao Zhou <2130606191@qq.com>
2026-05-01 21:56:09 +0800

    Update lib.rs

--- crates/hook-dll/src/lib.rs
+++ crates/hook-dll/src/lib.rs
@@ -1,8 +1,13 @@
 #[cfg(windows)]
+mod pipe_client;
+#[cfg(windows)]
 mod winhttp;
 
 #[cfg(windows)]
 use std::ffi::c_void;
+
+#[cfg(windows)]
+static mut G_HOOKS_INSTALLED: bool = false;
 
 #[cfg(windows)]
 #[unsafe(no_mangle)]
@@ -10,8 +15,31 @@ pub extern "system" fn DllMain(_hinst: *mut c_void, reason: u32, _reserved: *mut
     match reason {
         1 => {
             std::thread::spawn(|| {
-                tracing::info!("FlowReveal hook DLL attached");
+                unsafe {
+                    if G_HOOKS_INSTALLED {
+                        return;
+                    }
+                    G_HOOKS_INSTALLED = true;
+                }
+
+                match winhttp::install() {
+                    Ok(()) => {
+                        tracing::info!("FlowReveal hook DLL: WinHTTP hooks installed");
+                    }
+                    Err(e) => {
+                        tracing::error!("FlowReveal hook DLL: Failed to install hooks: {}", e);
+                    }
+                }
             });
+        }
+        0 => {
+            unsafe {
+                if G_HOOKS_INSTALLED {
+                    winhttp::uninstall();
+                    pipe_client::disconnect();
+                    G_HOOKS_INSTALLED = false;
+                }
+            }
         }
         _ => {}
     }
