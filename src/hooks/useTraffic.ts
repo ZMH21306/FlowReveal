4cfb1888e32b7e18b375434df2e2a5854eb4de1b
MingHao Zhou <2130606191@qq.com>
2026-05-02 13:01:15 +0800

    Update useTraffic.ts

--- src/hooks/useTraffic.ts
+++ src/hooks/useTraffic.ts
@@ -1,24 +1,18 @@
 import { listen } from "@tauri-apps/api/event";
 import { useEffect } from "react";
-import type { HttpMessage, EngineStats } from "../types";
+import type { HttpMessage } from "../types";
 import { useStore } from "../store";
 
 export function useTraffic() {
   const processMessage = useStore((s) => s.processMessage);
-  const setStats = useStore((s) => s.setStats);
 
   useEffect(() => {
     const unlistenRequest = listen<HttpMessage>("traffic:request", (event) => {
       processMessage(event.payload);
     });
 
-    const unlistenStats = listen<EngineStats>("traffic:stats", (event) => {
-      setStats(event.payload);
-    });
-
     return () => {
       unlistenRequest.then((fn) => fn());
-      unlistenStats.then((fn) => fn());
     };
-  }, [processMessage, setStats]);
+  }, [processMessage]);
 }
