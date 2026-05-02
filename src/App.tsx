626a02ca2e6e85fa057cbe3e791b7d5ed79f080c
MingHao Zhou <2130606191@qq.com>
2026-05-02 22:06:12 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { AppShell } from "./components/layout/AppShell";
 import { useTraffic } from "./hooks/useTraffic";
+import { useKeyboardShortcuts } from "./hooks/useKeyboardShortcuts";
 import "./styles/globals.css";
 
 export default function App() {
   useTraffic();
+  useKeyboardShortcuts();
   return <AppShell />;
 }
