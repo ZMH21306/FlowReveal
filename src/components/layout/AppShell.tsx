9eea8e2a8f20263293f68a11635ec50f2fa97cf1
MingHao Zhou <2130606191@qq.com>
2026-05-02 10:39:27 +0800

    Update AppShell.tsx

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,12 +1,14 @@
 import { Toolbar } from "./Toolbar";
 import { StatusBar } from "./StatusBar";
 import { TrafficList } from "../traffic/TrafficList";
+import { FilterBar } from "../traffic/FilterBar";
 import { RequestDetail } from "../detail/RequestDetail";
 
 export function AppShell() {
   return (
     <div className="flex flex-col h-screen w-screen bg-[var(--color-bg-primary)]">
       <Toolbar />
+      <FilterBar />
       <div className="flex flex-1 overflow-hidden">
         <div className="w-[55%] border-r border-[var(--color-border)] overflow-hidden">
           <TrafficList />
