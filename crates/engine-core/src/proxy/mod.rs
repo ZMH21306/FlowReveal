b9c7fda35ed006bac81f5fb9a2de07c08dc247f0
MingHao Zhou <2130606191@qq.com>
2026-05-05 11:01:53 +0800

    Update mod.rs

--- crates/engine-core/src/proxy/mod.rs
+++ crates/engine-core/src/proxy/mod.rs
@@ -1,7 +1,7 @@
 pub mod forward_proxy;
-pub mod transparent_proxy;
 pub mod mitm_proxy;
+pub mod transparent_proxy;
 pub mod utils;
 
-pub use forward_proxy::ForwardProxyHandle;
-pub use transparent_proxy::TransparentProxyHandle;
+pub use forward_proxy::ForwardProxy;
+pub use transparent_proxy::TransparentProxy;
